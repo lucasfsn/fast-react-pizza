42b2ad37a2bd46b7de82dd95b60e5950db11a95c
Łukasz Nowosielski <luki424221@gmail.com>
2023-10-15 14:47:41 +0200

    Added routes and app layout

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,11 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off',
+    // 'react-refresh/only-export-components': [
+    //   'warn',
+    //   { allowConstantExport: true },
+    // ],
   },
-}
+};
