f24106e4f266b7feb7002b6f3413b20187f3b5b1
lukasz <luki424221@gmail.com>
2023-10-17 09:59:20 +0200

    Added styles to menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -7,8 +7,8 @@ function Menu() {
   console.log(menu);
 
   return (
-    <ul>
-      {menu.map(pizza => (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
